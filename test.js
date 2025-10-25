30462df8dc47fc62e38971f5cc5d2ebb0a13360e
Roopan Vishnu <roopanvishnu.work@gmail.com>
2025-10-25 17:49:27 +0530

    chgns

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 console.log(5+5 );
+
+function add (){
+    const a = 5;
+    const b =10;
+    const c = a+b
+    console.log(c);
+}
+add();
+function runTwice(fun){
+    fun();
+    fun();
+}
+
+runTwice(add);
